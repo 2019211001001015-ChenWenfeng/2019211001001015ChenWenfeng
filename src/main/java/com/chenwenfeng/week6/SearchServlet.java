3772ce90fa14c47c9d57d1b379920b4f041d7cca
feng <1376245413@qq.com>
2021-04-07 20:00:02 +0800

    Week6 Commit

--- src/main/java/com/chenwenfeng/week6/SearchServlet.java
+++ src/main/java/com/chenwenfeng/week6/SearchServlet.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @WebServlet(name = "SearchServlet",value = "/search")
 public class SearchServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String text = request.getParameter("txt");
+
+        String text = URLEncoder.encode(request.getParameter("txt"),"utf-8");
         String search = request.getParameter("search");
         if(text == "")
         {
